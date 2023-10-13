540d452ead0914028f4ae97c010c1a2e6d527d11
Mykhailo-Kovalchuk <contact.kovalchuk@gmail.com>
2023-10-13 14:37:30 +0200

    goit-js-try2-hw-07-commit 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+       <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+ </li>`;
+  })
+  .join("");
+
+gallery.insertAdjacentHTML("afterbegin", markup);
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+});
+
+const options = {
+  captionsData: "alt",
+  captionDelay: 250,
+
+  spinner: false,
+  close: false,
+
+  animationSpeed: 500,
+  widthRatio: 1,
+};
+
+const lightbox = new SimpleLightbox(".gallery a", options);
+lightbox.on("show.simplelightbox", () => {
+  console.log("Enjoy");
+});
